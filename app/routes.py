75c20ba08965e6a133c435241c89de8491fb06ec
Jedrek11 <jedrekpawlowski011@gmail.com>
2023-06-23 10:22:00 +0200

    "23.06.2023 JP part 1"

--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,59 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for, request
+from app import utils
+import os
+import json
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
 
 @app.route('/')
 def index():
     return render_template('index.html.jinja')
 
-@app.route('/extract')
+@app.route('/extract', methods=['POST', 'GET'])
 def extract():
+    if request.method == 'POST':
+        product_code = request.form.get('product_code')
+        url = f"https://www.ceneo.pl/{product_code}#tab=reviews"
+        opinions_all = []
+        while(url):
+            print(url)
+            respons = requests.get(url)
+            if  respons.status_code == requests.codes.ok:
+                page_dom = BeautifulSoup(respons.text, 'html.parser')
+                opinions = page_dom.select("div.js_product-review")
+                for opinion in opinions:
+                    single_opinion = {}
+                    for key, value in utils.selectors.items():
+                        single_opinion[key] = utils.get_tag_content(opinion, *value)
+                    opinions_all.append(single_opinion)
+            try:
+                url = "https://www.ceneo.pl"+utils.get_tag_content(page_dom,"a.pagination__next","href")
+            except TypeError:
+                url = None
+        try:
+            os.mkdir("./app/data")
+            os.mkdir("./app/data/opinions")
+        except FileExistsError:
+            pass
+        with open(f"./app/data/opinions/{product_code}.json", "w", encoding="UTF-8") as jf:
+            json.dump(opinions_all, jf, indent=4, ensure_ascii=False)
+        return redirect(url_for('product', code=product_code))
     return render_template('extract.html.jinja')
 
 @app.route('/products')
 def products():
     return render_template('products.html.jinja')
 
-@app.route('/product')
-def product():
-    return render_template('product.html.jinja')
+@app.route('/product/<code>')
+def product(code):
+    opinions = pd.read_json(f"./app/data/opinions/{code}.json")
+    return render_template('product.html.jinja', 
+                           product_code=code, 
+                           opinions=opinions.to_html(header=True,
+                                                     table_id="opinions",
+                                                     classes="table table-striped table-hover"))
 
 @app.route('/charts')
 def charts():
---
+++ app/utils.py
@@ -0,0 +1,26 @@
+def get_tag_content(ancestor, selector=None, attribute=None, return_list=False):
+    try:
+        if return_list:
+            return [tag.get_text().strip() for tag in ancestor.select(selector)]
+        if not selector and attribute:
+            return ancestor[attribute].strip()
+        if attribute:
+            return ancestor.select_one(selector)[attribute].strip()
+        return ancestor.select_one(selector).get_text().strip()
+    except (AttributeError, TypeError):
+        return None
+
+selectors = {
+    "opinion_id": (None, "data-entry-id"),
+    "author": ("span.user-post__author-name",),
+    "recommendation": ("span.user-post__author-recomendation > em",),
+    "stars": ("span.user-post__score-count",),
+    "purchased": ("div.review-pz",),
+    "opinion_date": ("span.user-post__published > time:nth-child(1)","datetime"),
+    "purchase_date": ("span.user-post__published > time:nth-child(2)","datetime"),
+    "usefull_count": ("button.vote-yes","data-total-vote"),
+    "unusefull_count": ("button.vote-no","data-total-vote"),
+    "content": ("div.user-post__text",),
+    "pros": ("div.review-feature__title--positives ~ div.review-feature__item", None, True),
+    "cons": ("div.review-feature__title--negatives ~ div.review-feature__item", None, True)
+}
